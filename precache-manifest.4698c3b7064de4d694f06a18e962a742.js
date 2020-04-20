db9b0ebc67d4654034a425ac4e86b2a107fb866e
Camila Balestrin <balestrin.camila@gmail.com>
2020-04-20 14:03:23 -0300

    Updates

---
+++ precache-manifest.4698c3b7064de4d694f06a18e962a742.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dc42e148ff1ecce8506220a4b56423",
+    "url": "/tv-show-search/index.html"
+  },
+  {
+    "revision": "a898200a75c480843cca",
+    "url": "/tv-show-search/static/css/2.4cf5f81b.chunk.css"
+  },
+  {
+    "revision": "4fa5e2509493e4fed7b7",
+    "url": "/tv-show-search/static/css/main.d3b49c3d.chunk.css"
+  },
+  {
+    "revision": "a898200a75c480843cca",
+    "url": "/tv-show-search/static/js/2.8cce3c31.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tv-show-search/static/js/2.8cce3c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa5e2509493e4fed7b7",
+    "url": "/tv-show-search/static/js/main.b368861e.chunk.js"
+  },
+  {
+    "revision": "493e6694e636e824c49d",
+    "url": "/tv-show-search/static/js/runtime-main.95f16bde.js"
+  },
+  {
+    "revision": "ac23d307242fc225ceccb0eb0f8993ac",
+    "url": "/tv-show-search/static/media/logo.ac23d307.svg"
+  }
+]);
