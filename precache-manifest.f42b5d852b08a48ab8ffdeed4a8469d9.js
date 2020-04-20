225b9714aaa612fd80482d58516f856acdc16868
Camila Balestrin <balestrin.camila@gmail.com>
2020-04-20 16:23:32 -0300

    Updates

---
+++ precache-manifest.f42b5d852b08a48ab8ffdeed4a8469d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ec202bf9f6bff2b2d99baccaf1cd84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a898200a75c480843cca",
+    "url": "/static/css/2.4cf5f81b.chunk.css"
+  },
+  {
+    "revision": "4fa5e2509493e4fed7b7",
+    "url": "/static/css/main.d3b49c3d.chunk.css"
+  },
+  {
+    "revision": "a898200a75c480843cca",
+    "url": "/static/js/2.8cce3c31.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8cce3c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa5e2509493e4fed7b7",
+    "url": "/static/js/main.b368861e.chunk.js"
+  },
+  {
+    "revision": "7c1c9335195b21bf3785",
+    "url": "/static/js/runtime-main.a1353783.js"
+  },
+  {
+    "revision": "ac23d307242fc225ceccb0eb0f8993ac",
+    "url": "/static/media/logo.ac23d307.svg"
+  }
+]);
